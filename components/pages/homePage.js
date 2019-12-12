deff4b533ad329182216f321d1501bab60ce1d3f
Quirionais <42272773+Quirionais@users.noreply.github.com>
2019-12-12 19:29:24 +0100

    Updates

---
+++ components/pages/homePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+
+class Homepage extends Component {
+  render() {
+    return (
+        <div>
+            Home page here
+            </div>
+    );
+  }
+}
+
+
+
+export default Homepage;
